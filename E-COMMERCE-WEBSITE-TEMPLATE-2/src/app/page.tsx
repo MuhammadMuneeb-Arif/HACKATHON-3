50ae8dc2159b95324e03a5781284503ba58cbf93
M.MUNEEB AHMED <161030327+MuhammadMuneeb-Arif@users.noreply.github.com>
2024-12-08 23:21:21 +0500

    Add files via upload

---
+++ E-COMMERCE-WEBSITE-TEMPLATE-2/src/app/page.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+
+import Footer2 from "@/components/footer2";
+import Navbar from "@/components/navbar2";
+import Navbar1 from "@/components/navbar1";
+export default function Home() {
+  return (
+    <div>
+
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <Navbar/>
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br /><br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <Navbar1/>
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    <br /><br />
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <Footer2/>
+    </div>
+  );
+}
